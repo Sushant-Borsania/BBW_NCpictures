50813cf493baf5e188722d6188ab8d9fca432072
Sushant <borsaniasushant@gmail.com>
2019-12-29 18:43:06 -0700

    complete navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+import "./App.scss";
+
+library.add(fab);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Switch>
+          {/* <Route exact path = "/" component={Main}/> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,72 @@
+import React, { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as Logo } from "../images/logo_photography.svg";
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+
+export default function Navigation() {
+  useEffect(() => {
+    const btn = document.getElementById("toggler");
+    btn.addEventListener("click", toggler);
+
+    function toggler() {
+      document.getElementById("navs").classList.toggle("hidden");
+    }
+  }, []);
+  return (
+    <nav className="nav">
+      <div className="nav_left">
+        <Logo />
+        <h2 className="nav_left__brand-name">NC</h2>
+      </div>
+      <div className="nav_middle">
+        <ul className="nav_link">
+          <NavLink to="/About" exact className="nav_link__item">
+            <li>About</li>
+          </NavLink>
+          <NavLink to="/Service" className="nav_link__item">
+            <li>Service</li>
+          </NavLink>
+          <NavLink to="/Project" className="nav_link__item">
+            <li>Projects</li>
+          </NavLink>
+          <NavLink to="/Blog" className="nav_link__item">
+            <li>Blog</li>
+          </NavLink>
+          <NavLink to="/Contact" className="nav_link__item">
+            <li>Contact</li>
+          </NavLink>
+        </ul>
+      </div>
+      <div className="nav_right">
+        <FontAwesomeIcon className="nav_right__icon" icon={["fab", "facebook"]} />
+        <FontAwesomeIcon className="nav_right__icon" icon={["fab", "twitter"]} />
+        <FontAwesomeIcon className="nav_right__icon" icon={["fab", "instagram"]} />
+      </div>
+      <div className="container_small">
+        <button className="toggler" id="toggler">
+          Menu
+        </button>
+        <nav className="nav_small">
+          <ul className="small-lists" id="navs">
+            <NavLink to="/About" className="nav_link__item small-list">
+              <li>About</li>
+            </NavLink>
+            <NavLink to="/Service" className="nav_link__item small-list">
+              <li>Service</li>
+            </NavLink>
+            <NavLink to="/Project" className="nav_link__item small-list">
+              <li>Projects</li>
+            </NavLink>
+            <NavLink to="/Blog" className="nav_link__item small-list">
+              <li>Blog</li>
+            </NavLink>
+            <NavLink to="/Contact" className="nav_link__item small-list">
+              <li>Contact</li>
+            </NavLink>
+          </ul>
+        </nav>
+      </div>
+    </nav>
+  );
+}
