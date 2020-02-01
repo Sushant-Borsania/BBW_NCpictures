f0ccd370fd292803538bdfe820c0f7c95d0f38ea
Sushant <borsaniasushant@gmail.com>
2020-02-01 16:36:52 -0700

    About section completed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import Hero from './Hero';
-import About from './About'
+import React from "react";
+import Hero from "./Hero";
+import About from "./About";
+import Services from "./Services";
 export default function Main() {
   return (
     <React.Fragment>
       <Hero />
       <About />
+      <Services />
     </React.Fragment>
-  )
+  );
 }
---
+++ src/components/Services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Services() {
+  return (
+    <div className="container container_services">
+      <div className="services_left">
+        
+      </div>
+      <div className="services_right">
+
+      </div>
+    </div>
+  )
+}
